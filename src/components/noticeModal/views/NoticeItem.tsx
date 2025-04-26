0c0cc02bb6f6a09638c5ed7dc739992e2141b445
letthem <suyeon3662@naver.com>
2025-04-26 13:59:07 +0900

    [LCR-53] chore: git 대소문자 오류 방지

--- src/components/noticeModal/views/NoticeItem.tsx
+++ src/components/noticeModal/views/NoticeItem.tsx
@@ -1,9 +1,20 @@
 import { NoticeItemType } from "@/types/NoticeItemType";
-import { formatTimestamp } from "@/utils/FormatTimestamp";
 
 type Props = Omit<NoticeItemType, "id">;
 
 // 알림: api 10분에 한번씩 요청하기
+
+function formatTimestamp(timestamp: string) {
+  const date = new Date(timestamp);
+
+  const year = String(date.getFullYear()).slice(2); // YY
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // MM
+  const day = String(date.getDate()).padStart(2, "0"); // DD
+  const hours = String(date.getHours()).padStart(2, "0"); // HH
+  const minutes = String(date.getMinutes()).padStart(2, "0"); // MM
+
+  return `${year}.${month}.${day} ${hours}:${minutes}`;
+}
 
 export default function NoticeItem({
   type,
