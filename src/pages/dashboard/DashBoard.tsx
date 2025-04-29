aa7e28ee7b5cae74407216320e414fb73ba3414d
yeongseoRyou <95837534+Y0ungse@users.noreply.github.com>
2025-04-29 09:59:56 +0900

    [LCR-60] fix: 대시보드 이번주 인기상품 UI 필터,드롭다운 분리

--- src/pages/dashboard/DashBoard.tsx
+++ src/pages/dashboard/DashBoard.tsx
@@ -4,10 +4,14 @@ import DashBoardVisitor from "@/pages/dashboard/views/DashBoardVisitor";
 export default function DashBoard() {
   return (
 <<<<<<< HEAD
+<<<<<<< HEAD
     <div className="w-[1360px] mx-auto flex flex-col gap-[70px] pt-[50px] pb-[86px]">
 =======
     <div className="w-[1360px] mx-auto flex flex-col gap-[70px]">
 >>>>>>> 252774d ([LCR-56] fix: 수정)
+=======
+    <div className="w-[1360px] mx-auto flex flex-col gap-[70px] pt-[50px] pb-[86px]">
+>>>>>>> b3739d8 ([LCR-60] fix: 대시보드 이번주 인기상품 UI 필터,드롭다운 분리)
       <div className="flex gap-[70px]">
         <DashBoardCongestion />
         <DashBoardVisitor />
