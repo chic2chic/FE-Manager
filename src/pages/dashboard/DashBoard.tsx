f61926ebdbae48ff8d009f1c048f9b445694244c
yeongseoRyou <95837534+Y0ungse@users.noreply.github.com>
2025-04-29 16:13:02 +0900

    [LCR-56] feat: 대시보드 팝업스토어 방문자 분석 UI 구현

--- src/pages/dashboard/DashBoard.tsx
+++ src/pages/dashboard/DashBoard.tsx
@@ -1,9 +1,16 @@
 import DashBoardCongestion from "@/pages/dashboard/views/DashBoardCongestion";
+import DashBoardVisitorAnalysis from "./views/DashBoardVisitor";
 
 export default function DashBoard() {
   return (
     <div className="w-[1360px] mx-auto flex flex-col gap-[70px]">
-      <DashBoardCongestion />
+      {/* (선택) 다른 섹션이 있다면 여기 */}
+
+      {/*  이 wrapper가 flex-row 역할을 합니다.  */}
+      <div className="flex gap-[70px]">
+        <DashBoardCongestion />
+        <DashBoardVisitorAnalysis />
+      </div>
     </div>
   );
 }
