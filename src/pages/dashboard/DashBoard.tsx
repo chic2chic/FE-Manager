882fba59a2bbb7b644b94d65d15df20c80854e2b
yeongseoRyou <95837534+Y0ungse@users.noreply.github.com>
2025-04-29 22:55:20 +0900

    [LCR-60] fix: title 추가, 변수명 변경

--- src/pages/dashboard/DashBoard.tsx
+++ src/pages/dashboard/DashBoard.tsx
@@ -1,20 +1,16 @@
 import DashBoardCongestion from "@/pages/dashboard/views/DashBoardCongestion";
-import DashBoardVisitor from "@/pages/dashboard/views/DashBoardVisitor";
+import DashBoardCustomerTransaction from "@/pages/dashboard/views/DashBoardCustomerTransaction";
+import DashBoardReservation from "@/pages/dashboard/views/DashBoardReservation";
+import BestProduct from "./views/BestProduct";
 
 export default function DashBoard() {
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
     <div className="w-[1360px] mx-auto flex flex-col gap-[70px] pt-[50px] pb-[86px]">
-=======
-    <div className="w-[1360px] mx-auto flex flex-col gap-[70px]">
->>>>>>> 252774d ([LCR-56] fix: 수정)
-=======
-    <div className="w-[1360px] mx-auto flex flex-col gap-[70px] pt-[50px] pb-[86px]">
->>>>>>> b3739d8 ([LCR-60] fix: 대시보드 이번주 인기상품 UI 필터,드롭다운 분리)
-      <div className="flex gap-[70px]">
-        <DashBoardCongestion />
-        <DashBoardVisitor />
+      <BestProduct />
+      <DashBoardCongestion />
+      <div className="flex gap-10">
+        <DashBoardReservation />
+        <DashBoardCustomerTransaction />
       </div>
     </div>
   );
