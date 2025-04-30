37f5931a747ba5fd3e0dd49c91a3b73c24475c19
yeongseoRyou <95837534+Y0ungse@users.noreply.github.com>
2025-04-30 11:15:54 +0900

    [LCR-56] feat: 대시보드 방문자 분석 UI 구현

--- src/mocks/handlers/dashboard/VisitorDatas.ts
+++ src/mocks/handlers/dashboard/VisitorDatas.ts
@@ -1,9 +1,9 @@
-export const visitorGenderData = [
+export const VisitorGenderData = [
   { name: "남성", value: 70 },
   { name: "여성", value: 30 },
 ];
 
-export const visitorAgeData = [
+export const VisitorAgeData = [
   { name: "10대", value: 25 },
   { name: "20대", value: 35 },
   { name: "30대", value: 25 },
--- src/pages/dashboard/DashBoard.tsx
+++ src/pages/dashboard/DashBoard.tsx
@@ -2,12 +2,16 @@ import DashBoardCongestion from "@/pages/dashboard/views/DashBoardCongestion";
 import DashBoardCustomerTransaction from "@/pages/dashboard/views/DashBoardCustomerTransaction";
 import DashBoardReservation from "@/pages/dashboard/views/DashBoardReservation";
 import BestProduct from "./views/BestProduct";
+import DashBoardVisitor from "./views/DashBoardVisitor";
 
 export default function DashBoard() {
   return (
     <div className="w-[1360px] mx-auto flex flex-col gap-[70px] pt-[50px] pb-[86px]">
       <BestProduct />
-      <DashBoardCongestion />
+      <div className="flex gap-[70px]">
+         <DashBoardCongestion />
+         <DashBoardVisitor />
+       </div>
       <div className="flex gap-10">
         <DashBoardReservation />
         <DashBoardCustomerTransaction />
