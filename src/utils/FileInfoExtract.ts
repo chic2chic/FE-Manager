2e17d7c0323629897646b28e69905d976b578af9
sung-yeop <jjabc9459@naver.com>
2025-05-05 16:30:20 +0900

    [LCR-86] fix: File 정보 추출 로직 수정

--- src/utils/FileInfoExtract.ts
+++ src/utils/FileInfoExtract.ts
@@ -2,7 +2,8 @@ export const FileInoExtract = (file: File) => {
   const typeRegex = /\/([a-z]+)/;
   const match = file.type.match(typeRegex);
   const extension = match ? match[1] : "";
-  const fileName = file.name;
 
-  return { fileName, extension };
+  const nameWithoutExt = file.name.split(".")[0];
+
+  return { fileName: nameWithoutExt, extension };
 };
