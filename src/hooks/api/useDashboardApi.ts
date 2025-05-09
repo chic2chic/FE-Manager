d2e13c196721b5e13e73955194cd817ce7371af9
yeongseoRyou <95837534+Y0ungse@users.noreply.github.com>
2025-05-09 13:52:52 +0900

    [LCR-110] fix: 반환 방법 수정

--- src/hooks/api/useDashboardApi.ts
+++ src/hooks/api/useDashboardApi.ts
@@ -1,14 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAvgPurchase } from "@/apis/DashBoardApi";
-import type { GetAvgPurchaseResponse } from "@/types/api/ApiResponseType";
 
-export function useDashboardApi() {
-  return useQuery<GetAvgPurchaseResponse, Error>({
+export const useDashboardApi = () => {
+  const { data, isError, isLoading } = useQuery({
     queryKey: ["avgPurchase"],
     queryFn: async () => {
       const res = await getAvgPurchase();
       return res.data;
     },
-    retry: 1,
   });
-}
+
+  return { data, isError, isLoading };
+};
--- src/pages/dashboardPage/views/DashBoardCustomerTransaction.tsx
+++ src/pages/dashboardPage/views/DashBoardCustomerTransaction.tsx
@@ -3,33 +3,32 @@ import { CountCard } from "@/pages/dashboardPage/views/CountCard";
 import DashBoardTitle from "@/pages/dashboardPage/views/DashBoardTitle";
 
 export default function DashBoardCustomerTransaction() {
-  const { data, isLoading, isError, error } = useDashboardApi();
-
-  if (isLoading) return <div>Loading…</div>;
-  if (isError) return <div>Error: {String(error)}</div>;
-
-  const { totalPrice, todayPrice } = data!;
+  const { data } = useDashboardApi();
 
   return (
     <div className="w-[414px] flex-col">
       <DashBoardTitle title="1인 평균 구매액" />
       <div className="flex h-[394px] flex-col justify-between">
-        <CountCard
-          title="팝업 기간 내"
-          bgCSS="bg-mint02"
-          value={totalPrice.toLocaleString()}
-          valueCSS="text-mint08 text-[56px]"
-          unit="원"
-          unitCSS="text-mint08 text-[40px]"
-        />
-        <CountCard
-          title="TODAY"
-          bgCSS="bg-blue02"
-          value={todayPrice.toLocaleString()}
-          valueCSS="text-blue07 text-[56px]"
-          unit="원"
-          unitCSS="text-blue07 text-[40px]"
-        />
+        {data && (
+          <>
+            <CountCard
+              title="팝업 기간 내"
+              bgCSS="bg-mint02"
+              value={data.totalPrice.toLocaleString()}
+              valueCSS="text-mint08 text-[56px]"
+              unit="원"
+              unitCSS="text-mint08 text-[40px]"
+            />
+            <CountCard
+              title="TODAY"
+              bgCSS="bg-blue02"
+              value={data.todayPrice.toLocaleString()}
+              valueCSS="text-blue07 text-[56px]"
+              unit="원"
+              unitCSS="text-blue07 text-[40px]"
+            />
+          </>
+        )}
       </div>
     </div>
   );
