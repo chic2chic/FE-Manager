989d6adb56f235e8f36da164e80aa7280b8cf8f9
sung-yeop <jjabc9459@naver.com>
2025-05-09 15:47:07 +0900

    [LCR-113] fix: 팝업 아이디를 동적으로 할당하도록 수정

--- src/apis/ItemListApi.ts
+++ src/apis/ItemListApi.ts
@@ -1,15 +1,15 @@
 import { ApiResponse, GetItemListResponse } from "@/types/api/ApiResponseType";
 import { api } from "./config/Axios";
+import { usePopUpReadStore } from "@/stores/usePopUpReadStore";
 
 export const getItemList = async (): ApiResponse<GetItemListResponse> => {
-  // const popUpId = usePopUpCreateStore.getState(state => state.popUpId);
-  const popupId = 1;
+  const popupId = usePopUpReadStore.getState().popupId;
   const response = await api.get(`/popups/${popupId}/items`);
   return response.data;
 };
 
 export const deleteItem = async (itemId: string) => {
-  const popupId = 1;
+  const popupId = usePopUpReadStore.getState().popupId;
   const response = await api.delete(`/popups/${popupId}/items/${itemId}`);
   return response.data;
 };
