e251875bd7baeee6cb20eacd4ccf50481951a0d7
yeongseoRyou <95837534+Y0ungse@users.noreply.github.com>
2025-05-09 23:04:24 +0900

    [LCR-117] feat: 예약자 수 조회 기능 구현

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -3,9 +3,12 @@ import { AuthHandlers } from "@/mocks/handlers/onBoarding/Auth.handlers";
 import { DashboardHandlers } from "./handlers/dashboard/CustomerTransactionDatas";
 import { PopUpCreateHandlers } from "@/mocks/handlers/popUpCreate/PopUpCreate.handlers";
 import { popUpListReadHandlers } from "@/mocks/handlers/popUpListRead/popUpListRead.handlers";
-import { EntrantsHandlers } from "./handlers/dashboard/ReservationDatas";
-import { ItemListHandlers } from "./handlers/itemList/ItemList.handlers";
 import { ItemCreateHandlers } from "./handlers/itemCreate/ItemCreate.handlers";
+import {
+  EntrantsHandlers,
+  ReservationsHandlers,
+} from "./handlers/dashboard/ReservationDatas";
+import { ItemListHandlers } from "./handlers/itemList/ItemList.handlers";
 
 export const handlers = [
   ...AuthHandlers,
@@ -15,6 +18,7 @@ export const handlers = [
   ...ItemListHandlers,
   ...DashboardHandlers,
   ...EntrantsHandlers,
+  ...ReservationsHandlers,
 ];
 
 export const worker = setupWorker(...handlers);
