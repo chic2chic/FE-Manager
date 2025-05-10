adbf1a5d27252aeae619f67db7d256ad4eed8aeb
letthem <suyeon3662@naver.com>
2025-05-10 09:52:32 +0900

    [LCR-98] fix: build 에러 해결 (대소문자 이슈)

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,11 +1,11 @@
 import { setupWorker } from "msw/browser";
 import { AuthHandlers } from "@/mocks/handlers/onBoarding/Auth.handlers";
 import { ItemCreateHandlers } from "@/mocks/handlers/ItemCreate/ItemCreate.handlers";
-import { DashboardHandlers } from "./handlers/dashboard/CustomerTransactionDatas";
+import { DashboardHandlers } from "@/mocks/handlers/dashboard/CustomerTransactionDatas";
 import { PopUpCreateHandlers } from "@/mocks/handlers/popUpCreate/PopUpCreate.handlers";
 import { popUpListReadHandlers } from "@/mocks/handlers/popUpListRead/popUpListRead.handlers";
-import { ItemListHandlers } from "./handlers/itemList/ItemListRead.handlers";
-import { StockNotificationListHandlers } from "@/mocks/handlers/noticeModal/stockNotificationList.handlers";
+import { ItemListHandlers } from "@/mocks/handlers/itemList/ItemListRead.handlers";
+import { StockNotificationListHandlers } from "@/mocks/handlers/noticeModal/StockNotificationList.handlers";
 
 export const handlers = [
   ...AuthHandlers,
