fa984f4212ee57052a06eff7350e31a67998f464
sung-yeop <jjabc9459@naver.com>
2025-05-11 14:31:56 +0900

    [LCR-98] fix: 디렉토리명 대소문자 구분

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -3,13 +3,13 @@ import { AuthHandlers } from "@/mocks/handlers/onBoarding/Auth.handlers";
 import { DashboardHandlers } from "./handlers/dashboard/CustomerTransactionDatas";
 import { PopUpCreateHandlers } from "@/mocks/handlers/popUpCreate/PopUpCreate.handlers";
 import { popUpListReadHandlers } from "@/mocks/handlers/popUpListRead/popUpListRead.handlers";
-import { ItemCreateHandlers } from "./handlers/itemCreate/ItemCreate.handlers";
 import {
   EntrantsHandlers,
   ReservationsHandlers,
 } from "./handlers/dashboard/ReservationDatas";
-import { ItemListHandlers } from "./handlers/itemList/ItemList.handlers";
+import { ItemCreateHandlers } from "./handlers/itemCreate/ItemCreate.handlers";
 import { StockNotificationListHandlers } from "@/mocks/handlers/noticeModal/StockNotificationList.handlers";
+import { ItemListHandlers } from "./handlers/itemList/ItemList.handlers";
 
 export const handlers = [
   ...AuthHandlers,
