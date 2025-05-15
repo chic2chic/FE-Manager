a18f0e130653f9728a5181de88b7117705039d37
sung-yeop <jjabc9459@naver.com>
2025-05-15 11:08:52 +0900

    [LCR-145] feat: 로딩 컴포넌트 추가

--- src/pages/popUpListPage/PopUpListPage.tsx
+++ src/pages/popUpListPage/PopUpListPage.tsx
@@ -13,9 +13,10 @@ import bin from "@/assets/webps/common/bin.webp";
 import check from "@/assets/webps/common/check.webp";
 import { usePopUpListReadApi } from "@/hooks/api/usePopUpListReadApi";
 import { usePopUpDeleteApi } from "@/hooks/api/usePopUpDeleteApi";
+import Loading from "@/components/common/Loading";
 
 export default function PopUpListPage() {
-  const { cards } = usePopUpListReadApi();
+  const { cards, isLoading } = usePopUpListReadApi();
   const { deletePopUpMutation } = usePopUpDeleteApi();
   const navigate = useNavigate();
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
@@ -70,64 +71,70 @@ export default function PopUpListPage() {
         {/* swiper */}
         <div className="flex justify-center mt-12">
           <div className="relative mx-auto w-[1296px]">
-            <Swiper
-              className="popup-swiper w-[1010px] mx-auto"
-              centeredSlides={true}
-              slidesPerView={3}
-              spaceBetween={72}
-              loop={true}
-              navigation={{
-                prevEl: ".custom-prev",
-                nextEl: ".custom-next",
-              }}
-              modules={[Pagination, Navigation]}
-            >
-              {cards && cards.length > 0 ? (
-                cards.map(card => (
-                  <SwiperSlide
-                    key={card.popupId}
-                    className="h-[342px] w-[286px] flex flex-col justify-center items-center"
-                  >
-                    <PopUpCard
-                      popupId={card.popupId}
-                      name={card.name}
-                      imageUrl={card.imageUrl}
-                      onDeleteClick={() => handleDelete(card.popupId)}
-                    />
-                  </SwiperSlide>
-                ))
-              ) : (
-                <div className="flex flex-col items-center justify-center">
-                  <p className="mt-18 text-[32px] text-gray10 font-medium">
-                    등록된 팝업이 없습니다.
-                  </p>
-                  <p className="mt-4 text-[20px] text-gray10">
-                    우측 상단 <span className="text-main06">+ 버튼</span>을 눌러
-                    팝업을 등록해주세요!
-                  </p>
-                </div>
-              )}
-            </Swiper>
-            {/* prev & next 버튼 */}
-            {cards && cards.length > 0 ? (
-              <>
-                <div className="custom-prev absolute top-[110px] -translate-y-1/2 left-0 z-10 cursor-pointer">
-                  <img
-                    src={leftArrowGray09}
-                    alt="prev"
-                    className="w-[35px] h-[35px]"
-                  />
-                </div>
-                <div className="custom-next absolute top-[110px] -translate-y-1/2 right-0 z-10 cursor-pointer">
-                  <img
-                    src={rightArrowGray09}
-                    alt="next"
-                    className="w-[35px] h-[35px]"
-                  />
-                </div>
-              </>
+            {isLoading ? (
+              <div className="flex justify-center items-center h-[342px]">
+                <Loading />
+              </div>
             ) : (
-              <></>
+              <>
+                <Swiper
+                  className="popup-swiper w-[1010px] mx-auto"
+                  centeredSlides={true}
+                  slidesPerView={3}
+                  spaceBetween={72}
+                  loop={true}
+                  navigation={{
+                    prevEl: ".custom-prev",
+                    nextEl: ".custom-next",
+                  }}
+                  modules={[Pagination, Navigation]}
+                >
+                  {cards && cards.length > 0 ? (
+                    cards.map(card => (
+                      <SwiperSlide
+                        key={card.popupId}
+                        className="h-[342px] w-[286px] flex flex-col justify-center items-center"
+                      >
+                        <PopUpCard
+                          popupId={card.popupId}
+                          name={card.name}
+                          imageUrl={card.imageUrl}
+                          onDeleteClick={() => handleDelete(card.popupId)}
+                        />
+                      </SwiperSlide>
+                    ))
+                  ) : (
+                    <div className="flex flex-col items-center justify-center">
+                      <p className="mt-18 text-[32px] text-gray10 font-medium">
+                        등록된 팝업이 없습니다.
+                      </p>
+                      <p className="mt-4 text-[20px] text-gray10">
+                        우측 상단 <span className="text-main06">+ 버튼</span>을
+                        눌러 팝업을 등록해주세요!
+                      </p>
+                    </div>
+                  )}
+                </Swiper>
+                {/* prev & next 버튼 */}
+                {cards && cards.length > 0 && (
+                  <>
+                    <div className="custom-prev absolute top-[110px] -translate-y-1/2 left-0 z-10 cursor-pointer">
+                      <img
+                        src={leftArrowGray09}
+                        alt="prev"
+                        className="w-[35px] h-[35px]"
+                      />
+                    </div>
+                    <div className="custom-next absolute top-[110px] -translate-y-1/2 right-0 z-10 cursor-pointer">
+                      <img
+                        src={rightArrowGray09}
+                        alt="next"
+                        className="w-[35px] h-[35px]"
+                      />
+                    </div>
+                  </>
+                )}
+              </>
             )}
           </div>
         </div>
