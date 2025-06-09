d7f4f5f32072e37033abd7e26e1998273edf6d9a
sung-yeop <jjabc9459@naver.com>
2025-06-09 23:12:17 +0900

    [LCR-324] chore: Lighthouse CI 설정 [pref]

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import prettier from "eslint-plugin-prettier";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "scripts/*"] },
   {
     files: ["eslint.config.js", "*.js"],
     ...js.configs.recommended,
