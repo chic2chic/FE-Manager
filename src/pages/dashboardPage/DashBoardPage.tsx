30ccd39ccaa7d158c21a86bc0bb7e971ab58a257
letthem <suyeon3662@naver.com>
2025-06-10 13:06:31 +0900

    [perf] test

--- src/pages/dashboardPage/DashBoardPage.tsx
+++ src/pages/dashboardPage/DashBoardPage.tsx
@@ -8,7 +8,7 @@ import DashBoardQuestionnaire from "@/pages/dashboardPage/views/DashBoardQuestio
 
 export default function DashBoardPage() {
   return (
-    // 테스트
+    // 테스트입니다
     <div className="w-[1360px] mx-auto flex flex-col gap-[70px] pt-[50px] pb-[86px]">
       <BestProduct />
       <div className="flex gap-[70px]">
