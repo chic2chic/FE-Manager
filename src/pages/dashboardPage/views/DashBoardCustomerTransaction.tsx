c61fca3c45b0232fed1a5904dc5c9afa98b203ea
letthem <suyeon3662@naver.com>
2025-06-11 13:57:10 +0900

    [LCR-334] refactor: QueryComponent 사용하여 예약 분석, 1인 평균 구매액, 설문지 분석, 구매전환율 Skeleton, NoDataComp 처리

--- src/pages/dashboardPage/views/DashBoardCustomerTransaction.tsx
+++ src/pages/dashboardPage/views/DashBoardCustomerTransaction.tsx
@@ -2,38 +2,71 @@ import { useAvgPurchaseApi } from "@/hooks/api/useDashboardApi";
 import { CountCard } from "@/pages/dashboardPage/views/CountCard";
 import DashBoardTitle from "@/pages/dashboardPage/views/DashBoardTitle";
 import NoDataComp from "@/components/common/NoDataComp";
+import Skeleton from "@/components/common/Skeleton";
+import { QueryComponent } from "@/components/common/QueryComponent";
 
 export default function DashBoardCustomerTransaction() {
-  const { data } = useAvgPurchaseApi();
+  const { data, isLoading, isError } = useAvgPurchaseApi();
 
   return (
     <div className="w-[414px] flex-col" data-testid="dashboard-transaction">
       <DashBoardTitle title="1인 평균 구매액" />
       <div className="flex h-[394px] flex-col justify-between">
-        {data ? (
-          <>
+        {/* 팝업 기간 내 */}
+        <QueryComponent
+          data={data?.totalAverageAmount}
+          isLoading={isLoading}
+          isError={isError}
+          loadingFallback={
+            <div className="h-[180px] rounded-[40px] bg-gray02 flex justify-center items-center overflow-hidden">
+              <Skeleton />
+            </div>
+          }
+          emptyFallback={
+            <div className="h-[180px] rounded-[40px] bg-gray02 flex justify-center items-center">
+              <NoDataComp />
+            </div>
+          }
+        >
+          {amount => (
             <CountCard
               title="팝업 기간 내"
               bgCSS="bg-mint02"
-              value={data.totalAverageAmount}
+              value={amount}
               valueCSS="text-mint08 text-[56px]"
               unit="원"
               unitCSS="text-mint08 text-[40px]"
             />
+          )}
+        </QueryComponent>
+
+        {/* TODAY */}
+        <QueryComponent
+          data={data?.todayAverageAmount}
+          isLoading={isLoading}
+          isError={isError}
+          loadingFallback={
+            <div className="h-[180px] rounded-[40px] bg-gray02 flex justify-center items-center overflow-hidden">
+              <Skeleton />
+            </div>
+          }
+          emptyFallback={
+            <div className="h-[180px] rounded-[40px] bg-gray02 flex justify-center items-center">
+              <NoDataComp />
+            </div>
+          }
+        >
+          {amount => (
             <CountCard
               title="TODAY"
               bgCSS="bg-blue02"
-              value={data.todayAverageAmount}
+              value={amount}
               valueCSS="text-blue07 text-[56px]"
               unit="원"
               unitCSS="text-blue07 text-[40px]"
             />
-          </>
-        ) : (
-          <div className="flex justify-center h-[394px] bg-gray02 rounded-[50px]">
-            <NoDataComp />
-          </div>
-        )}
+          )}
+        </QueryComponent>
       </div>
     </div>
   );
--- src/pages/dashboardPage/views/DashBoardQuestionnaire.tsx
+++ src/pages/dashboardPage/views/DashBoardQuestionnaire.tsx
@@ -8,6 +8,7 @@ import { QuestionnaireResponse } from "@/types/api/ApiResponseType";
 import { Questions } from "@/constants/popUpCreate/Questions";
 import NoDataComp from "@/components/common/NoDataComp";
 import Skeleton from "@/components/common/Skeleton";
+import { QueryComponent } from "@/components/common/QueryComponent";
 
 const options = ["1번 문항", "2번 문항", "3번 문항", "4번 문항"];
 const SIZE = 300;
@@ -56,7 +57,7 @@ const isZeroTotal = (questionnaireData: QuestionnaireResponse): boolean => {
 
 export default function DashBoardQuestionnaire() {
   const [selectedQuestion, setSelectedQuestion] = useState(options[0]);
-  const { surveys, isLoading } = useQuestionnaireApi();
+  const { surveys, isLoading, isError } = useQuestionnaireApi();
 
   const handleQuestion = (selected: string) => {
     setSelectedQuestion(selected);
@@ -67,8 +68,6 @@ export default function DashBoardQuestionnaire() {
     const target = Questions.find(q => q.questionNumber === questionNumber);
     return `Q${target?.questionNumber}. ${target?.title}`;
   };
-
-  const matched = matchQA(selectedQuestion, surveys);
 
   return (
     <div
@@ -85,132 +84,156 @@ export default function DashBoardQuestionnaire() {
           />
         </div>
       </div>
-      {isLoading ? (
-        <div className="w-[1360px] h-[662px] rounded-[50px] overflow-hidden">
-          <Skeleton />
-        </div>
-      ) : !matched ? (
-        <div className="w-[1360px] h-[662px] bg-gray02 rounded-[50px]">
-          <NoDataComp />
-        </div>
-      ) : (
-        <div className="w-[1360px] h-[662px] bg-gray02 rounded-[50px] px-[60px] py-[45px]">
-          <p className="font-semibold text-[36px]">
-            {matchQuestion(selectedQuestion)}
-          </p>
-          <div className="flex">
-            <div className="w-[1240px] h-[490px] bg-gray01 mt-[40px] rounded-[40px] px-[40px] py-[30px] flex justify-between">
-              {/* 왼쪽 - 답변 리스트 */}
-              <div className="flex flex-col gap-[20px] w-[458px]">
-                {matched.contents.map((content, idx) => (
-                  <div key={idx}>
-                    <div className="rounded-[20px] bg-gray02 text-[20px] flex items-center h-[72px] w-full">
-                      <span className="mr-4 w-[64px] h-full flex items-center justify-center rounded-l-[20px] text-[32px] bg-blue02">
-                        {idx + 1}
-                      </span>
-                      <p className="text-[20px] ml-[30px]">{content.title}</p>
-                    </div>
-                  </div>
-                ))}
+
+      <QueryComponent
+        data={surveys}
+        isLoading={isLoading}
+        isError={isError}
+        loadingFallback={
+          <div className="w-[1360px] h-[662px] rounded-[50px] overflow-hidden">
+            <Skeleton />
+          </div>
+        }
+        emptyFallback={
+          <div className="w-[1360px] h-[662px] bg-gray02 rounded-[50px]">
+            <NoDataComp />
+          </div>
+        }
+      >
+        {data => {
+          const matched = matchQA(selectedQuestion, data);
+          if (!matched || matched.contents.length === 0) {
+            return (
+              <div className="w-[1360px] h-[662px] bg-gray02 rounded-[50px] flex items-center justify-center">
+                <NoDataComp />
               </div>
-              {/* 오른쪽 - 파이 차트 */}
-              <div className="flex flex-col items-end h-full">
-                <p className="text-[20px] font-medium text-gray08">
-                  전체 응시자 수: {matched.total}명
-                </p>
-                <div className="flex justify-center gap-[80px] items-center">
-                  {isZeroTotal(matched) ? (
-                    // 모든 항목의 값이 0인 경우 메시지 표시
-                    <div className="flex items-center justify-center w-[300px] h-[300px]">
-                      <p className="text-[20px] text-gray08 text-center">
-                        아직 응답한 데이터가 없습니다.
-                      </p>
-                    </div>
-                  ) : (
-                    // 정상적인 파이 차트 표시
-                    <PieChart width={SIZE} height={SIZE}>
-                      <Tooltip
-                        cursor={{ fill: "transparent" }}
-                        content={(props: TooltipProps<number, string>) => {
-                          if (
-                            !props.active ||
-                            !props.payload ||
-                            props.payload.length === 0
-                          )
-                            return null;
-
-                          const targetPayload = props.payload[0].payload;
-                          return (
-                            <CustomTooltip
-                              active={props.active}
-                              payload={[
-                                {
-                                  value: targetPayload.selectedCount,
-                                  name: targetPayload.title,
-                                  dataKey: "selectedCount",
-                                },
-                              ]}
-                              label={targetPayload.title}
-                              unitSuffix="명"
-                              highlightColor={
-                                colorSet[
-                                  props.payload[0].dataKey === "selectedCount"
-                                    ? matched.contents.findIndex(
-                                        c => c.title === targetPayload.title,
-                                      ) % colorSet.length
-                                    : 0
-                                ]
-                              }
-                            />
-                          );
-                        }}
-                      />
-
-                      <Pie
-                        data={matched.contents}
-                        innerRadius={0}
-                        outerRadius={RADIUS}
-                        dataKey="selectedCount"
-                        nameKey="title"
-                        cx="50%"
-                        cy="50%"
-                        paddingAngle={0}
-                      >
-                        {matched.contents.map((_, idx) => (
-                          <Cell
-                            key={idx}
-                            fill={colorSet[idx % colorSet.length]}
-                          />
-                        ))}
-                      </Pie>
-                    </PieChart>
-                  )}
-                  <div className="grid grid-cols-2 gap-x-6 gap-y-4 h-[100px]">
+            );
+          }
+          return (
+            <div className="w-[1360px] h-[662px] bg-gray02 rounded-[50px] px-[60px] py-[45px]">
+              <p className="font-semibold text-[36px]">
+                {matchQuestion(selectedQuestion)}
+              </p>
+              <div className="flex">
+                <div className="w-[1240px] h-[490px] bg-gray01 mt-[40px] rounded-[40px] px-[40px] py-[30px] flex justify-between">
+                  {/* 왼쪽 - 답변 리스트 */}
+                  <div className="flex flex-col gap-[20px] w-[458px]">
                     {matched.contents.map((content, idx) => (
-                      <div key={idx} className="flex items-center gap-2">
-                        <span
-                          className="w-[38px] h-[18px] rounded-[8px]"
-                          style={{
-                            backgroundColor: colorSet[idx % colorSet.length],
-                          }}
-                        />
-                        <span className="flex gap-2">
-                          <span>{idx + 1}번</span>
-                          {isZeroTotal(matched) && (
-                            <span className="text-gray06">
-                              ({content.selectedCount}명)
-                            </span>
-                          )}
-                        </span>
+                      <div key={idx}>
+                        <div className="rounded-[20px] bg-gray02 text-[20px] flex items-center h-[72px] w-full">
+                          <span className="mr-4 w-[64px] h-full flex items-center justify-center rounded-l-[20px] text-[32px] bg-blue02">
+                            {idx + 1}
+                          </span>
+                          <p className="text-[20px] ml-[30px]">
+                            {content.title}
+                          </p>
+                        </div>
                       </div>
                     ))}
+                  </div>
+                  {/* 오른쪽 - 파이 차트 */}
+                  <div className="flex flex-col items-end h-full">
+                    <p className="text-[20px] font-medium text-gray08">
+                      전체 응시자 수: {matched.total}명
+                    </p>
+                    <div className="flex justify-center gap-[80px] items-center">
+                      {isZeroTotal(matched) ? (
+                        // 모든 항목의 값이 0인 경우 메시지 표시
+                        <div className="flex items-center justify-center w-[300px] h-[300px]">
+                          <p className="text-[20px] text-gray08 text-center">
+                            아직 응답한 데이터가 없습니다.
+                          </p>
+                        </div>
+                      ) : (
+                        // 정상적인 파이 차트 표시
+                        <PieChart width={SIZE} height={SIZE}>
+                          <Tooltip
+                            cursor={{ fill: "transparent" }}
+                            content={(props: TooltipProps<number, string>) => {
+                              if (
+                                !props.active ||
+                                !props.payload ||
+                                props.payload.length === 0
+                              )
+                                return null;
+
+                              const targetPayload = props.payload[0].payload;
+                              return (
+                                <CustomTooltip
+                                  active={props.active}
+                                  payload={[
+                                    {
+                                      value: targetPayload.selectedCount,
+                                      name: targetPayload.title,
+                                      dataKey: "selectedCount",
+                                    },
+                                  ]}
+                                  label={targetPayload.title}
+                                  unitSuffix="명"
+                                  highlightColor={
+                                    colorSet[
+                                      props.payload[0].dataKey ===
+                                      "selectedCount"
+                                        ? matched.contents.findIndex(
+                                            c =>
+                                              c.title === targetPayload.title,
+                                          ) % colorSet.length
+                                        : 0
+                                    ]
+                                  }
+                                />
+                              );
+                            }}
+                          />
+
+                          <Pie
+                            data={matched.contents}
+                            innerRadius={0}
+                            outerRadius={RADIUS}
+                            dataKey="selectedCount"
+                            nameKey="title"
+                            cx="50%"
+                            cy="50%"
+                            paddingAngle={0}
+                          >
+                            {matched.contents.map((_, idx) => (
+                              <Cell
+                                key={idx}
+                                fill={colorSet[idx % colorSet.length]}
+                              />
+                            ))}
+                          </Pie>
+                        </PieChart>
+                      )}
+                      <div className="grid grid-cols-2 gap-x-6 gap-y-4 h-[100px]">
+                        {matched.contents.map((content, idx) => (
+                          <div key={idx} className="flex items-center gap-2">
+                            <span
+                              className="w-[38px] h-[18px] rounded-[8px]"
+                              style={{
+                                backgroundColor:
+                                  colorSet[idx % colorSet.length],
+                              }}
+                            />
+                            <span className="flex gap-2">
+                              <span>{idx + 1}번</span>
+                              {isZeroTotal(matched) && (
+                                <span className="text-gray06">
+                                  ({content.selectedCount}명)
+                                </span>
+                              )}
+                            </span>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      )}
+          );
+        }}
+      </QueryComponent>
     </div>
   );
 }
