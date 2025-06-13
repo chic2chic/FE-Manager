53a8eabcca6c7999ad7cfe81bd98148f3a3dd039
letthem <suyeon3662@naver.com>
2025-06-13 10:13:12 +0900

    [LCR-350] refactor: CT(Customer Transaction) 코드 리팩토링

--- src/pages/dashboardPage/views/customerTransaction/index.tsx
+++ src/pages/dashboardPage/views/customerTransaction/index.tsx
@@ -8,49 +8,50 @@ import QueryComponent from "@/components/common/QueryComponent";
 const CustomerTransaction = () => {
   const { data, isLoading, isError } = useAvgPurchaseApi();
 
+  const cardConfigs = [
+    {
+      key: "total",
+      title: "팝업 기간 내",
+      value: data?.totalAverageAmount,
+      bgCSS: "bg-mint02",
+      valueCSS: "text-mint08 text-[56px]",
+      unitCSS: "text-mint08 text-[40px]",
+    },
+    {
+      key: "today",
+      title: "TODAY",
+      value: data?.todayAverageAmount,
+      bgCSS: "bg-blue02",
+      valueCSS: "text-blue07 text-[56px]",
+      unitCSS: "text-blue07 text-[40px]",
+    },
+  ];
+
   return (
     <div className="w-[414px] flex-col" data-testid="dashboard-transaction">
       <Title title="1인 평균 구매액" />
       <div className="flex h-[394px] flex-col justify-between">
-        {/* 팝업 기간 내 */}
-        <QueryComponent
-          data={data?.totalAverageAmount}
-          isLoading={isLoading}
-          isError={isError}
-          loadingFallback={<Skeleton height="h-[180px]" />}
-          emptyFallback={<NoDataComp height="h-[180px]" />}
-        >
-          {data => (
-            <CountCard
-              title="팝업 기간 내"
-              bgCSS="bg-mint02"
-              value={data}
-              valueCSS="text-mint08 text-[56px]"
-              unit="원"
-              unitCSS="text-mint08 text-[40px]"
-            />
-          )}
-        </QueryComponent>
-
-        {/* TODAY */}
-        <QueryComponent
-          data={data?.totalAverageAmount}
-          isLoading={isLoading}
-          isError={isError}
-          loadingFallback={<Skeleton height="h-[180px]" />}
-          emptyFallback={<NoDataComp height="h-[180px]" />}
-        >
-          {data => (
-            <CountCard
-              title="TODAY"
-              bgCSS="bg-blue02"
-              value={data}
-              valueCSS="text-blue07 text-[56px]"
-              unit="원"
-              unitCSS="text-blue07 text-[40px]"
-            />
-          )}
-        </QueryComponent>
+        {cardConfigs.map(config => (
+          <QueryComponent
+            key={config.key}
+            data={config.value}
+            isLoading={isLoading}
+            isError={isError}
+            loadingFallback={<Skeleton height="h-[180px]" />}
+            emptyFallback={<NoDataComp height="h-[180px]" />}
+          >
+            {value => (
+              <CountCard
+                title={config.title}
+                bgCSS={config.bgCSS}
+                value={value}
+                valueCSS={config.valueCSS}
+                unit="원"
+                unitCSS={config.unitCSS}
+              />
+            )}
+          </QueryComponent>
+        ))}
       </div>
     </div>
   );
